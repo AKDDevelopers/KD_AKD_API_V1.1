a54f2bb258af30591a15fa14c22babcda44d8a1d
Teckchun <teckchunaing@gmail.com>
2017-04-22 11:06:19 +0700

    fix server p12 file path

--- src/main/java/org/khmeracademy/akd/services/impl/UploadFolderToGoogleServiceImpl.java
+++ src/main/java/org/khmeracademy/akd/services/impl/UploadFolderToGoogleServiceImpl.java
@@ -34,7 +34,7 @@ public class UploadFolderToGoogleServiceImpl {
 		//	ALL KHMER DOCS GOOGLE DRIVE API
 		String serviceAccountID="all-khmer-docs@all-khmer-docs-146405.iam.gserviceaccount.com";
 //		String ServiceAccountPrivateKey="ALL-KHMER-DOCS-4ef8850572e9.p12";
-		String ServiceAccountPrivateKey="/opt/FILES_MANAGEMENT/ALL_KHMER_DOCS/ALL-KHMER-DOCS-4ef8850572e9.p12";
+		String ServiceAccountPrivateKey="/Data/PROJECTS/FILES_MANAGEMENT/ALL_KHMER_DOCS/ALL-KHMER-DOCS-4ef8850572e9.p12";
 		///opt/FILE MANAGEMENTS/
 		String con = parentID.toLowerCase();
 		
